e4c1ec585284f4fa5ea6927b2579df0127fe3be7
YahiaTr352 <yahiatr188@gmail.com>
2025-05-23 23:19:44 +0300

    final xx commit

--- controllers/potato.js
+++ controllers/potato.js
@@ -268,7 +268,7 @@ const {
 } = require('../utils/encryption');
 const paymentData = require("../models/paymentDataModel");
 
-BASE_API_URL = "https://projectone-wqlf.onrender.com"
+BASE_API_URL = "https://projectone-q5eh.onrender.com"
 // BASE_API_URL = "http://localhost:5000";
 // ======== API Handlers ========
 
--- public/js/otpVerification.js
+++ public/js/otpVerification.js
@@ -13,7 +13,7 @@ function setLoadingState(isLoading) {
 const pathParts = window.location.pathname.split("/");
 const publicID = pathParts[pathParts.length - 1];
 window.addEventListener("DOMContentLoaded", async () => {
-  const baseURL = "https://projecttwo-r5gg.onrender.com";
+  const baseURL = "https://projecttwo-iqjp.onrender.com";
     // const baseURL = "http://localhost:3001";
   let fixedData;
     try{

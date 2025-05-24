994d9bdffc2e51b0dbc703bba8ec7b32ae1a011f
YahiaTr352 <yahiatr188@gmail.com>
2025-05-24 23:44:25 +0300

    final xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx commit

--- app.js
+++ app.js
@@ -18,10 +18,12 @@ const port = process.env.PORT || 3001;
 app.disable("x-powered-by");
 app.use(userAgentFilter);
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true
-}));
+// app.use(cors({
+//   origin: "http://localhost:3000",
+//   credentials: true
+// }));
+
+app.use(cors());
 
 app.use(express.json());
 ConnectDB();
